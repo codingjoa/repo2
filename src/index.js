37cfb11bbac420e2017e353fdabdcdd585d375d9
divdivcount <49908463+divdivcount@users.noreply.github.com>
2020-08-11 21:39:59 +0900

    gvds

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * from './LinkButton';
+import LinkButton from './LinkButton';
 
 ReactDOM.render(
   <React.StrictMode>
