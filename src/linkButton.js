fccc515bfbe76ff8d36e83f1b8a8cc087a7d6762
divdivcount <49908463+divdivcount@users.noreply.github.com>
2020-08-11 21:32:14 +0900

    T^T

--- src/linkButton.js
+++ src/linkButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// 위에 코드 생략(파일이름 linkButton.js로 저장)
+// 위에 코드 생략(파일이름 LinkButton.js로 저장)
 class LinkButton extends React.Component{
   constructor(props){
     super(props);
@@ -13,4 +13,4 @@ class LinkButton extends React.Component{
     // return e('button' , {onClick:()=>{this.setState({liked:true})}, type:'submit'} ,this.state.liked === true ? 'Liked' : 'Like', );//<button>Like</button>
   }
 }
-module.export = { linkButton };
+module.export = { LinkButton };
