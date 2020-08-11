260028a98938d5ff304b91530f6f63b41320c3e1
divdivcount <49908463+divdivcount@users.noreply.github.com>
2020-08-11 21:13:33 +0900

    Ha..

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import LinkButton from './LinkButton';
+import LinkButton from './linkButton';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <LinkButton />
+    <LikeButton />
   </React.StrictMode>,
   document.getElementById('root')
 );
