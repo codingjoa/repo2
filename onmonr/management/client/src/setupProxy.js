09dec6a28f1568ed5be2d3e285e61aee19d60aea
divdivcount <jang05812002@naver.com>
2020-08-24 23:07:25 +0900

    rr

--- onmonr/management/client/src/setupProxy.js
+++ onmonr/management/client/src/setupProxy.js
@@ -4,9 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const exampleProxy = createProxyMiddleware('/api', {...});
 
 module.exports = function(app) {
-  const exampleProxy = createProxyMiddleware('/api', {...});
   app.use(proxy("/api/costomers", { target: "http://localhost:5000",changeOrigin:true}));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
