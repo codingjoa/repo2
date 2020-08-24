2b17dd6142935976358d8712010b67b4cc3b4a97
divdivcount <jang05812002@naver.com>
2020-08-24 20:22:49 +0900

    ㄱㄱ

--- onmonr/management/client/src/App.js
+++ onmonr/management/client/src/App.js
@@ -169,6 +169,13 @@ class App extends Component {
       });
       return data.map((c) => {
         return <Customer className={classes.tableBody} stateRefresh={this.stateRefresh}  key={c.sid} sid={c.sid} qid={c.qid} name={c.name} age={c.age} birthday={c.birthday} gender={c.gender} phone={c.phone} email={c.email} address={c.address} uniqueness={c.uniqueness}  />
+      });
+    }
+    const filterdComponents2 = (data2) => {
+      data2 = data2.filter((c)=>{
+        return c.name.indexOf(this.state.searchKeyword) > -1;
+      });
+      return data2.map((c) => {
         return <Student_check className={classes.tableBody} stateRefresh2={this.stateRefresh2}  key={c.cid} cid = {c.cid} sid={c.sid} qid={c.qid} tid={c.tid} name={c.name} date_time={c.date_time} />
       });
     }
