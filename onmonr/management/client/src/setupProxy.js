3db24b410ce5cad9c546093e16e8cafc359b367b
divdivcount <jang05812002@naver.com>
2020-08-24 22:29:29 +0900

    ㅗㅡ..

--- onmonr/management/client/src/setupProxy.js
+++ onmonr/management/client/src/setupProxy.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 5000;
 
 module.exports = function(app) {
   app.use(proxy("/api/costomers", { target: "http://localhost:5000" }));
+  app.use(proxy("/api/studentcheck", { target: "http://localhost:5000" }));
+  
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
 
