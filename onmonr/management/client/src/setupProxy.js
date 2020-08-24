e32931f8e97f7fb9f5fb80f34bfc3a8d368d94b3
divdivcount <jang05812002@naver.com>
2020-08-24 23:36:30 +0900

    gkagfdnadsfglk

--- onmonr/management/client/src/setupProxy.js
+++ onmonr/management/client/src/setupProxy.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const proxy  = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 
 module.exports = function(app) {
-  app.use(proxy("/api/", { target: "http://localhost:5000"}));
+  app.use(createProxyMiddleware("/api", { target: "http://localhost:5000",changeOrigin: true}));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
   const data = fs.readFileSync('../database.json');
