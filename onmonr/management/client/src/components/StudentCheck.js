6e7924ae36a941ed803cc32f3bc459ed7b3d2f8e
divdivcount <jang05812002@naver.com>
2020-08-25 20:56:36 +0900

    r

--- onmonr/management/client/src/components/StudentCheck.js
+++ onmonr/management/client/src/components/StudentCheck.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import Button from '@material-ui/core/Button';
 
 const checkStyle = {
   textAlign:"center"
