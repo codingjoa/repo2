96ae2590b892bdc52bcde5d1c0850d725a6b1dda
divdivcount <jang05812002@naver.com>
2020-08-25 20:43:15 +0900

    r

--- onmonr/management/client/src/components/StudentCheck.js
+++ onmonr/management/client/src/components/StudentCheck.js
@@ -6,14 +6,13 @@ const checkStyle = {
   textAlign:"center"
 
 }
-let t = Date.parse({{this.props.date_time}});
-let time = new Date(t);
-console.log(time.toString());
-
 class StudentCheck extends React.Component{
 
 
     render(){
+      let t = Date.parse({this.props.date_time});
+      let time = new Date(t);
+      console.log(time.toString());
         return(
             <TableRow style={checkStyle}>
                 <TableCell style={checkStyle}>{this.props.cid}</TableCell>
