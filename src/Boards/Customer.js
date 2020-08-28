663cc5ec73bc88a39ba766fa35980ed18fc8c56a
codingjoa <taek55@gmail.com>
2020-08-28 18:24:49 +0900

    1234

--- src/Boards/Customer.js
+++ src/Boards/Customer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import CustomerDelete from './CustomerDelete';
@@ -7,30 +7,36 @@ import axios from 'axios';
 
 export default function Customer (props) {
   const [ datas, setDatas ] = useState(null);
-  const Remover = useCallback(sid => {
+  const Refresh = useCallback(sid => {
+    setDatas(null);
     axios.delete(`/api/db/${sid}`).then(() => null).then(setDatas);
-  });
+  }, []);
   if(!datas) {
     axios.get('/api/db').then(r => r.data).then(setDatas);
-    return null;
+    return (
+      <>자료를 다시 불러오는 중</>
+    );
   }
   return (
     <>
       {datas.map(row =>
       <TableRow style={{textAlign:"center"}}>
-      <TableCell style={{textAlign:"center"}}></TableCell>
-      <TableCell style={{textAlign:"center"}}>{row.sid}</TableCell>
-      <TableCell style={{textAlign:"center"}}>{row.qid}</TableCell>
-      <TableCell style={{textAlign:"center"}}>{row.name}</TableCell>
-      <TableCell style={{textAlign:"center"}}>{row.age}</TableCell>
-      <TableCell style={{textAlign:"center"}}>{row.birthday}</TableCell>
-      <TableCell style={{textAlign:"center"}}>{row.gender}</TableCell>
-      <TableCell style={{textAlign:"center"}}>{row.phone}</TableCell>
-      <TableCell style={{textAlign:"center"}}>{row.email}</TableCell>
-      <TableCell style={{textAlign:"center"}}>{row.address}</TableCell>
-      <TableCell style={{textAlign:"center"}}>{row.uniqueness}</TableCell>
-      <TableCell style={{textAlign:"center"}}><CustomerDelete id={row.sid} Remover={Remover}/></TableCell>
-    </TableRow>
+        <TableCell style={{textAlign:"center"}}></TableCell>
+        <TableCell style={{textAlign:"center"}}>{row.sid}</TableCell>
+        <TableCell style={{textAlign:"center"}}>{row.qid}</TableCell>
+        <TableCell style={{textAlign:"center"}}>{row.name}</TableCell>
+        <TableCell style={{textAlign:"center"}}>{row.age}</TableCell>
+        <TableCell style={{textAlign:"center"}}>{row.birthday}</TableCell>
+        <TableCell style={{textAlign:"center"}}>{row.gender}</TableCell>
+        <TableCell style={{textAlign:"center"}}>{row.phone}</TableCell>
+        <TableCell style={{textAlign:"center"}}>{row.email}</TableCell>
+        <TableCell style={{textAlign:"center"}}>{row.address}</TableCell>
+        <TableCell style={{textAlign:"center"}}>{row.uniqueness}</TableCell>
+        <TableCell style={{textAlign:"center"}}>
+          { /* 삭제 버튼임 */ }
+          <CustomerDelete id={row.sid} refresh={Refresh}/>
+        </TableCell>
+      </TableRow>
       )}
     </>
   );
